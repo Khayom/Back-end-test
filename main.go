7c8585838f7fcebe7945db07848724b5c14c0588
Your Name <you@example.com>
2024-03-04 18:20:49 +0500

    1

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Khayom Backend!")
+	fmt.Println("Hello, Khayom Backend")
 	fmt.Println("Hello, world! - 2")
 	fmt.Println("Hello, world! - 3")
 	fmt.Println("Hello, world!")
